1aee554bda7a23470a326244e7180e172b689d4a
BarthaSzabolcs <barthaszabolcs.job@gmail.com>
2018-10-17 19:51:32 +0100

    Almost fixed. :|

--- WormsClone/Assets/_Scripts/DataScripts/ExplosionData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Data/Explosion")]
-public class ExplosionData : ScriptableObject
-{
-	public Sprite[] animationFrames;
-}

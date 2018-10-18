75acf8539956c6a7c6242eb466fc9fecb290619c
BarthaSzabolcs <barthaszabolcs.job@gmail.com>
2018-10-18 10:24:27 +0100

    Szabolcs - Fixed

---
+++ BaseDefense/Assets/_Scripts/DataScripts/HealthData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Health")]
+public class HealthData : ScriptableObject
+{
+    [Header("Health:")]
+	public int maxHealthPoints;
+
+    [Header("Hit:")]
+    public string hitAudio;
+    public bool flashOnHealthChange;
+    public float flashTime = 1f;
+
+    [Header("Death:")]
+    public string deathAudio;
+    public GameObject deathAnim;
+    public ExplosionData deathAnimData;
+}
---
+++ BaseDefense/Assets/_Scripts/Explosion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    #region HideInEditor
+    [HideInInspector] public ExplosionData data;
+    SpriteRenderer sRenderer;
+    #endregion
+
+    private void Start()
+	{
+		StartCoroutine(PlayAnimation());
+	}
+	IEnumerator PlayAnimation()
+	{
+        if (data.isExplosive)
+        {
+            var circleCollider = gameObject.AddComponent<CircleCollider2D>();
+            circleCollider.radius = data.explosionRange;
+            circleCollider.isTrigger = true;
+            circleCollider.usedByEffector = true;
+
+            var pointEffector = gameObject.AddComponent<PointEffector2D>();
+            pointEffector.useColliderMask = true;
+            pointEffector.colliderMask = data.explosionMask;
+            pointEffector.forceMagnitude = data.explosionMagnitude;
+            pointEffector.forceSource = EffectorSelection2D.Collider;
+            pointEffector.forceMode = data.explosionForceMode;
+        }
+        sRenderer = GetComponent<SpriteRenderer>();
+		for (int i = 0; i < data.animationFrames.Length; i++)
+		{
+			sRenderer.sprite = data.animationFrames[i];
+            yield return new WaitForEndOfFrame();
+		}
+		Destroy(gameObject);
+	}
+}

e9c6b83b756ed2449274d5dc1c87385771c4f652
Unknown <adamber95@gmail.com>
2018-11-25 15:39:06 +0100

    Spawn Manager Fixed, Working on AI-s.
    Spawn manager wavek randomok, minden mennyiségű ai-ra működik, játék végét is kezeli ha az összes játékban megjelenő AI-n rajta van a "Is_AI_Destroyed" script ( így fog működni vagy csinálj amit akarsz ).

---
+++ BaseDefense/Assets/_Scripts/Is_AI_Destroyed.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Is_AI_Destroyed : MonoBehaviour
+{
+    private void OnDestroy()
+    {
+        SpawnManager_B_Adam.enemiesAlive -= 1;
+    }
+}
